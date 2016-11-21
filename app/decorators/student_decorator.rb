8d23fcee361a0b6b3ec181e4cc8b50753d3ba541
JPeikert <jpeikert@gmail.com>
2016-11-22 00:34:57 +0100

    Add birthdate to Student in DB and views

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -7,4 +7,8 @@ class StudentDecorator < BaseDecorator
     notes = subject_item_notes.where(subject_item_id: subject_item.id).collect(&:value)
     notes.empty? ? "0.00" : "%.2f" % (notes.inject(:+).to_f/notes.size)
   end
+
+  def birth_date
+    birthdate.strftime("%Y-%m_%d") if birthdate
+  end
 end
