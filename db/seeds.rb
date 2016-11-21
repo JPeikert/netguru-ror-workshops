1094cbd0990cf38ec8ac19a94e912dd180967c5d
JPeikert <jpeikert@gmail.com>
2016-11-21 23:36:11 +0100

    Fix seeds for Heroku
    Could be done by placing gem Faker into production, but it's probably better
    not to feed production with fake data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,32 +2,46 @@ puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
-3.times do
-  Teacher.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    academic_title: TEACHER_TITLES.sample
-  )
-end
+Teacher.create!(first_name: "Retha", last_name: "Gerhold", academic_title: TEACHER_TITLES.sample)
+Teacher.create!(first_name: "John", last_name: "Roman", academic_title: TEACHER_TITLES.sample)
+Teacher.create!(first_name: "Lynn", last_name: "Zimmermann", academic_title: TEACHER_TITLES.sample)
 
 teachers = Teacher.all
-5.times do
-  SubjectItem.create!(
-    title: Faker::Lorem.sentence,
-    teacher: teachers.sample
-  )
-end
+SubjectItem.create!(title: "Mathematics", teacher: teachers.sample)
+SubjectItem.create!(title: "Biology", teacher: teachers.sample)
+SubjectItem.create!(title: "History", teacher: teachers.sample)
+SubjectItem.create!(title: "Geology", teacher: teachers.sample)
+SubjectItem.create!(title: "Quantum mechanics", teacher: teachers.sample)
 
-25.times do
-  Student.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
-  )
-end
+Student.create!(first_name: "John", last_name: "Cena")
+Student.create!(first_name: "Johnny", last_name: "Bravo")
+Student.create!(first_name: "Margaret", last_name: "Smith")
+Student.create!(first_name: "Jaylan", last_name: "Homenick")
+Student.create!(first_name: "Mabel", last_name: "Glover")
+Student.create!(first_name: "Makenna", last_name: "Collins")
+Student.create!(first_name: "Ned", last_name: "Labadie")
+Student.create!(first_name: "Rosalyn", last_name: "Flatley")
+Student.create!(first_name: "Efren", last_name: "Krajcik")
+Student.create!(first_name: "Merritt", last_name: "Trantow")
+Student.create!(first_name: "Lorenzo", last_name: "Hermann")
+Student.create!(first_name: "Junius", last_name: "Sporer")
+Student.create!(first_name: "Marcos", last_name: "Stiedemann")
+Student.create!(first_name: "Caleb", last_name: "Hills")
+Student.create!(first_name: "Pat", last_name: "Schumm")
+Student.create!(first_name: "Elyse", last_name: "Bednar")
+Student.create!(first_name: "Lavern", last_name: "Hyatt")
+Student.create!(first_name: "Jayne", last_name: "Lebsack")
+Student.create!(first_name: "Samanta", last_name: "Kilback")
+Student.create!(first_name: "Dejuan", last_name: "Kihn")
+Student.create!(first_name: "Laurine", last_name: "Gleichner")
+Student.create!(first_name: "Angela", last_name: "Haley") 
+Student.create!(first_name: "Clark", last_name: "Halvorson")
+Student.create!(first_name: "Peter", last_name: "Dare")
+Student.create!(first_name: "Angus", last_name: "MacGyver")
 
 students = Student.all
 SubjectItem.all.each do |subject_item|
-  subject_item.students << students.sample(rand(1..4))
+  subject_item.students << students.sample(rand(4..10))
 end
 
 SubjectItem.all.each do |subject_item|
